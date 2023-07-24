eab09f0c54d2f7d7bf8d938f1e99bb7ef7044f6c
Oleksandr Romaniuk <oleksandr.romaniuk@protonmail.com>
2023-07-24 13:25:57 +0300

    try to fix

--- core/settings.py
+++ core/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Media files
 MEDIA_ROOT = BASE_DIR / 'media'
