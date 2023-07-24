9949592e48957e8c7fa7cef59c4210c862a10272
Oleksandr Romaniuk <oleksandr.romaniuk@protonmail.com>
2023-07-24 13:29:16 +0300

    rm cahching statics

--- core/settings.py
+++ core/settings.py
@@ -139,7 +139,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STORAGES = {
     # ...
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
